9d97bb23c5376448bf575c3dca8bf434b418a2fb
palmor7 <premtsis_7@hotmail.com>
2018-11-09 01:47:19 +0200

    testing modals with bootstrap and angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { user } from './userModel';
+import $ from 'node_modules/jquery';
+// declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'modaltest';
+export class AppComponent implements OnInit{
+  user: user[] = [];
+
+  registerUser(registerForm: NgForm){
+    let newUser: user= {
+      amka: registerForm.value.registerAmka,
+      firstName: registerForm.value.registerfName,
+      lastName: registerForm.value.registerlName,
+      email: registerForm.value.registerEmail,
+      userName: registerForm.value.registerUsername,
+      password: registerForm.value.registerPassword,
+      phone: registerForm.value.registerPhone
+    }
+    
+    console.log(newUser);
+    registerForm.reset();
+    $("#registerModal").modal("hide");
+    //$("#registerModal .close").click();
+    // $('.modal.in').modal('hide');
+    
+  }
+
+  ngOnInit(){
+
+  }
 }
---
+++ src/app/userModel.js
@@ -0,0 +1,9 @@
+export class user{
+    amka: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    userName: string,
+    password: string,
+    phone: number
+}
